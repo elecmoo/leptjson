8acd5576e144d84ecc2fc100b4f12ad7aad3ee58
elecmoo <clsun4p@qq.com>
2022-09-19 00:49:10 +0800

    add function to parse string

--- leptjson.c
+++ leptjson.c
@@ -3,17 +3,36 @@
 #include <errno.h>
 #include <math.h>
 #include <string.h>
+#include <stdbool.h>
+
 #include "leptjson.h"
 
-#define EXPECT(ctx, ch) do { assert(*ctx->json == (ch)); ctx->json++; } while(0)
+#ifndef LEPT_PARSE_STACK_INIT_SIZE
+// 栈初始化大小
+#define LEPT_PARSE_STACK_INIT_SIZE 256
+#endif
 
-#define ISDIGIT(ch) ((ch) >= '0' && (ch) <= '9')
+// 判断字符是否为0-9
+static inline bool is_digit(char ch) {
+    return ch >= '0' && ch <= '9';
+}
 
-#define ISDIGIT1TO9(ch) ((ch) >= '1' && (ch) <= '9')
+// 判断字符是否为1-9
+static inline bool is_digit_1_to_9(char ch) {
+    return ch >= '1' && ch <= '9';
+}
 
 typedef struct {
     const char *json;
+    char *stack;
+    size_t size, top;
 } lept_context;
+
+// 判断字符串的首字母是否为预期值，不是则退出程序，是则将程序将指针后移一位
+static inline void expect(lept_context *ctx, char ch) {
+    assert(*ctx->json == ch);
+    ctx->json++;
+}
 
 static void lept_parse_whitespace(lept_context *c);
 
@@ -24,6 +43,9 @@ int lept_parse(lept_value *v, const char *json) {
     assert(v != NULL);
     int ret;
     c.json = json;
+    c.stack = NULL;
+    c.size = 0;
+    c.top = 0;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
     if ((ret = lept_parse_value(&c, v)) == LEPT_RETURN_PARSE_OK) {
@@ -35,6 +57,8 @@ int lept_parse(lept_value *v, const char *json) {
         }
 
     }
+    assert(c.top == 0); // 确保解析结束时，栈中所有的元素均被弹出
+    free(c.stack); // 解析结束释放栈内存
 
     return ret;
 }
@@ -48,7 +72,7 @@ static void lept_parse_whitespace(lept_context *c) {
 
 static int lept_parse_literal(lept_context *c, lept_value *v, const char *literal, lept_type type) {
     // 注意，EXPECT宏会将c->json指针向后移动一位
-    EXPECT(c, literal[0]);
+    expect(c, literal[0]);
 
     size_t i; // i是数组的索引，最好申明为size_t类型
     for (i = 0; literal[i + 1]; i++) {
@@ -70,19 +94,19 @@ static int lept_parse_number(lept_context *c, lept_value *v) {
     if (*p == '0')
         p++;
     else {
-        if (!ISDIGIT1TO9(*p))
+        if (!is_digit_1_to_9(*p))
             return LEPT_RETURN_PARSE_INVALID_VALUE;
         // 跳过后面的整数
-        for (p++; ISDIGIT(*p); p++);
+        for (p++; is_digit(*p); p++);
     }
     // 如果整数后面出现小数点，则跳过小数点，且后面至少跟一位整数
     if (*p == '.') {
         p++;
         // 小数点后面至少一位整数
-        if (!ISDIGIT(*p))
+        if (!is_digit(*p))
             return LEPT_RETURN_PARSE_INVALID_VALUE;
         // 跳过后面的整数
-        for (p++; ISDIGIT(*p); p++);
+        for (p++; is_digit(*p); p++);
     }
     // 判断指数部分
     if (*p == 'e' || *p == 'E') {
@@ -91,21 +115,11 @@ static int lept_parse_number(lept_context *c, lept_value *v) {
         if (*p == '-' || *p == '+')
             p++;
         // 后面至少跟一位整数
-        if (!ISDIGIT(*p))
+        if (!is_digit(*p))
             return LEPT_RETURN_PARSE_INVALID_VALUE;
         // 跳过后面的整数
-        for (p++; ISDIGIT(*p); p++);
+        for (p++; is_digit(*p); p++);
     }
-
-//    const char *pre, *p = c->json;
-//    for (; *p != '\0'; pre = p, p++) {
-//        if (!ISDIGIT(*p) && *p != '-' && *p != 'e' && *p != 'E' && *p != '.' && *p != '+')
-//            return LEPT_RETURN_PARSE_INVALID_VALUE;
-//        else if (p == c->json && (*p == '.' || *p == '+'))
-//            return LEPT_RETURN_PARSE_INVALID_VALUE;
-//    }
-//    if (*pre == '.')
-//        return LEPT_RETURN_PARSE_INVALID_VALUE;
 
     errno = 0; // 先将errno设置为0，成功
     v->n = strtod(c->json, NULL);
@@ -174,11 +188,13 @@ void lept_set_number(lept_value *v, double n) {
 }
 
 const char *lept_get_string(const lept_value *v) {
-    // TODO:
+    assert(v != NULL && v->type == LEPT_STRING);
+    return v->s;
 }
 
 size_t lept_get_string_length(const lept_value *v) {
-    // TODO:
+    assert(v != NULL && v->type == LEPT_STRING);
+    return v->len;
 }
 
 void lept_set_string(lept_value *v, const char *s, size_t len) {
@@ -191,3 +207,57 @@ void lept_set_string(lept_value *v, const char *s, size_t len) {
     v->len = len;
     v->type = LEPT_STRING;
 }
+
+static void *lept_context_push(lept_context *c, size_t size) {
+    void *ret;
+    if (c->top + size >= c->size) {
+        if (c->size == 0) {
+            c->size = LEPT_PARSE_STACK_INIT_SIZE;
+        }
+        while (c->top + size >= c->size) {
+            c->size += c->size >> 2; // 将c->size增加为原来的1.5倍
+        }
+        c->stack = realloc(c->stack, c->size); // 重新为stack分配内存
+    }
+    ret = c->stack + c->top;
+    c->top = c->top + size;
+    return ret;
+}
+
+static void *lept_context_pop(lept_context *c, size_t size) {
+    assert(c->top >= size);
+    c->top -= size;
+    return c->stack + c->top;
+}
+
+static inline void put_c(lept_context *c, char ch) {
+    // 先为字符分配内存，然后将字符存储到该位置
+    char *p = lept_context_push(c, sizeof(char));
+    *p = ch;
+}
+
+static int lept_parse_string(lept_context *c, lept_value *v) {
+    size_t head = c->top, len;
+    const char *p = c->json;
+    expect(c, '\"');
+    for (;;) {
+        char ch = *p++;
+        switch (ch) {
+            case '\"':
+                // 解析到json字符串末尾了
+                len = c->top - head;
+                // 从栈中弹出字符串，并将其设置到value中
+                const char *s = lept_context_pop(c, len);
+                lept_set_string(v, s, len);
+                c->json = p;
+                return LEPT_RETURN_PARSE_OK;
+            case '\0':
+                // 解析到c语言字符串的末尾也没有找到第二个双引号，解析失败
+                c->top = head; // 恢复栈的位置
+                return LEPT_RETURN_PARSE_MISS_QUOTATION_MARK;
+            default:
+                put_c(c, ch);
+        }
+    }
+}
+
